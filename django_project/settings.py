3d20a58e147959ee934787cb0f6d6c40aa2f73c5
Vladislav Pareiko <parieiko.vlad@gmail.com>
2023-04-15 21:59:31 +0400

    Add staticfiles environment

--- django_project/settings.py
+++ django_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-07f-dc5$_=^4fx#&8%iedo0-tz1#s^3v6cybo4c=!qvq)u-wz4"
+SECRET_KEY = os.environ["SECRET"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
